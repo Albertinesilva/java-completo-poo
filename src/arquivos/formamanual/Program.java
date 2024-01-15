169e58f2c3e53e1422e1033d9d10881223807f2a
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-01-15 17:19:49 -0300

    Forma Enchuta de Ler um Arquivo

---
+++ src/arquivos/formamanual/Program.java
@@ -0,0 +1,60 @@
+package arquivos.formamanual;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Program {
+
+  public static void main(String[] args) {
+
+    limparTela();
+    String path = "D:\\UDEMY(POO)\\in.txt";
+    FileReader fr = null;
+    BufferedReader br = null;
+
+    try {
+
+      fr = new FileReader(path);
+      br = new BufferedReader(fr);
+
+      String line = br.readLine();
+
+      while (line != null) {
+        System.out.println(line);
+        line = br.readLine();
+      }
+
+    } 
+    catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    } 
+    finally {
+      try {
+        if (br != null) {
+          br.close();
+        }
+        if (fr != null) {
+          fr.close();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/arquivos/trywithresources/Program.java
@@ -0,0 +1,41 @@
+package arquivos.trywithresources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Program {
+
+  public static void main(String[] args) {
+
+    limparTela();
+    String path = "D:\\UDEMY(POO)\\in.txt";
+
+    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+      String line = br.readLine();
+
+      while (line != null) {
+        System.out.println(line);
+        line = br.readLine();
+      }
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
+}
