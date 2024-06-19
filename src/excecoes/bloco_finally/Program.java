6fdb2197fc80654ff1a73d7678c9c66a0aa2c01e
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-06-19 17:22:55 -0300

    Finally

---
+++ src/excecoes/bloco_finally/Program.java
@@ -0,0 +1,49 @@
+package excecoes.bloco_finally;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+
+  public static void main(String[] args) {
+
+    limparTela();
+    
+    File file = new File("D:\\UDEMY(POO)\\exercicios\\src\\excecoes\\bloco_finally\\dados\\in.txt");
+    Scanner scan = null;
+
+    try {
+      scan = new Scanner(file);
+      while (scan.hasNextLine()) {
+        System.out.println(scan.nextLine());
+      }
+    } 
+    catch (FileNotFoundException e) {
+      System.out.println("Error opening file: " + e.getMessage());
+
+    } 
+    finally {
+      if (scan != null) {
+        scan.close();
+      }
+      System.out.println("Finally block executed");
+    }
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
+}
