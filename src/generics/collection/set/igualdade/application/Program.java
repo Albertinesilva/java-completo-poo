750389c6e0201d6b3c147be78f75dc1f1263ab22
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-12-29 20:52:53 -0300

    Igualdade com HashSet

---
+++ src/generics/collection/set/igualdade/application/Program.java
@@ -0,0 +1,38 @@
+package generics.collection.set.igualdade.application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import generics.collection.set.igualdade.entities.Product;
+
+public class Program {
+  public static void main(String[] args) {
+    limparTela();
+
+    Set<Product> set = new HashSet<>();
+
+    set.add(new Product("TV", 900.0));
+    set.add(new Product("Notebook", 1200.0));
+    set.add(new Product("Tablet", 400.0));
+
+    Product prod = new Product("Notebook", 1200.0);
+    
+    System.out.println("Contém 'Notebook'? " + set.contains(prod) + "\n");
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
+}
