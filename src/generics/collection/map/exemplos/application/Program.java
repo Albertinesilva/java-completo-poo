44e1d767a6981df486242c21db7f598140b4cffe
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-12-29 22:23:47 -0300

    Exemplo de Map utilizando com TreeMap<>()

---
+++ src/generics/collection/map/exemplos/application/Program.java
@@ -0,0 +1,45 @@
+package generics.collection.map.exemplos.application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+  public static void main(String[] args) {
+    limparTela();
+    Map<String, String> cookies = new TreeMap<>();
+
+    cookies.put("username", "maria");
+    cookies.put("email", "maria@gmail.com");
+    cookies.put("phone", "99771122");
+
+    cookies.remove("email");
+
+    cookies.put("phone", "99771133");
+
+    System.out.println("\nContains 'phone' key: " + cookies.containsKey("phone"));
+    System.out.println("Phone number: " + cookies.get("phone"));
+    System.out.println("Email: " + cookies.get("email"));
+    System.out.println("Size: " + cookies.size());
+
+    System.out.println("\nALL COOKIES:");
+    for (String key : cookies.keySet()) {
+      System.out.println(key + ": " + cookies.get(key));
+    }
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
+}
