8582c55080139b1c0a6294c4ce8d29d4c1ec2dfe
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-01-06 16:17:21 -0300

    Transparência Referencial

---
+++ src/expressoes_lambda/programacao_funcional/calculo_lambda/application/Program.java
@@ -0,0 +1,39 @@
+package expressoes_lambda.programacao_funcional.calculo_lambda.application;
+
+import java.util.Arrays;
+
+public class Program {
+  public static int globalValue = 3;
+
+  public static void main(String[] args) {
+    limparTela(); // Limpa a tela do console
+    
+    int[] vect = new int[] { 3, 4, 5 };
+    changeOddValues(vect); // Exemplo de função que não é referencialmente transparente
+    System.out.println(Arrays.toString(vect));
+  }
+
+  public static void changeOddValues(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 != 0) {
+        numbers[i] += globalValue;
+      }
+    }
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
+}
